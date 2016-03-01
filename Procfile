e98c4526d6afff8acd5d675f2662b0901c2ca4cd
Manaia Junior <dev.manaiajr@gmail.com>
2016-03-01 13:25:08 -0300

    Configura projeto para o heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn educarinova.wsgi --log-file -
--- educarinova/settings.py
+++ educarinova/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from decouple import config, Csv
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y)r$(lv3w4i!5*iax*v^r(h@s%6_8z4zece#mf3@l-ysjxc!2x'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 LOGIN_REDIRECT_URL = '/dashboard/'
 
@@ -77,11 +79,9 @@ WSGI_APPLICATION = 'educarinova.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
 
 
