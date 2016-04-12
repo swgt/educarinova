e1c07fdd4394b8476c3e01403bc79f587ab82f13
Manaia Junior <dev.manaiajr@gmail.com>
2016-04-12 15:04:49 -0300

    Ajuste na migração 002 no valor default

--- educarinova/management/migrations/0002_auto_20160406_1419.py
+++ educarinova/management/migrations/0002_auto_20160406_1419.py
@@ -33,7 +33,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='class',
             name='vagas',
-            field=models.CharField(default=0, max_length=10, verbose_name='vagas'),
+            field=models.CharField(default="0", max_length=10, verbose_name='vagas'),
         ),
         migrations.AddField(
             model_name='matriculation',
