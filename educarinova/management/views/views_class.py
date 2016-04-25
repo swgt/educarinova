ea59856027e6092ebb4c426839a763541902c35f
franklindias <franklindias99@gmail.com>
2016-04-25 17:15:38 -0300

    pesquisa por nome, ano letivo e turno na listagem de turmas

--- educarinova/management/views/views_class.py
+++ educarinova/management/views/views_class.py
@@ -1,13 +1,36 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
 from educarinova.management.models import Class, Matriculation
 from educarinova.management.forms.forms_class import ClassForm
 
 
 @login_required
 def list_class(request):
-    classes = Class.objects.all().order_by('-id')
-    return render(request, 'management/class/class_list.html', {'classes': classes})
+    query = request.GET.get('q')
+    if query is None:
+        query = ''
+
+    total_classes = Class.objects.count()
+    class_list = Class.objects.filter(Q(name__icontains=query) | Q(academic_year__icontains=query) |
+                                          Q(shift__icontains=query)).order_by('-id')
+
+    paginator = Paginator(class_list, 10)
+
+    page = request.GET.get('page')
+
+    try:
+        classv = paginator.page(page)
+    except PageNotAnInteger:
+        classv = paginator.page(1)
+    except EmptyPage:
+        classv = paginator.page(paginator.num_pages)
+
+    return render(request, 'management/class/class_list.html',
+                  {'classes': classv,
+                   'query': query,
+                   'total_classes': total_classes})
 
 
 @login_required
